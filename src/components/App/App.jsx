2516c0e7b8429f852cd6073a2b47399062ac2341
Roselin <roselin.t.m@gmail.com>
2025-03-20 12:23:29 -0400

    Header feat

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,16 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import Header from '../Header/Header'
 
 function App() {
   return (
-    <>
+    <div className="app">
+      <div className="app__content">
+        <Header/>
+      </div>
       
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+import logo from '../../images/logo.svg'
+import avatar from '../../images/avatar-logo.svg'
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__logo-container">
+            <img className="header__logo" src={logo} alt='logo'/>
+            <p className="header__date-and-location">Date, Location</p>
+            </div>
+            <div className="header__menu-container">
+                <button className="header__add-clothes-button">+ Add Clothes</button>
+                <p className="header__username">Terrence Tegegne</p>
+                <img className="header__avatar" src={avatar} alt='Terrence Tegegne'/>
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header;
